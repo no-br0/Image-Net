4cefcec64085e3604be0f2228e23d6f5cb255b24
no-br0 <alan_w81@hotmail.com>
2026-05-12 13:19:45 +0800

    rename layers_config to pattern_config

--- src/helpers/sync_input_config.py
+++ src/helpers/sync_input_config.py
@@ -1,7 +1,7 @@
 import os, json
 import numpy as np
 from Config.log_dir import INPUT_CONFIG_PATH
-from Config.layers_config import layers_cfg
+from Config.pattern_config import layers_cfg
 
 def sync_input_config(model_save_path):
 	if os.path.exists(model_save_path):
